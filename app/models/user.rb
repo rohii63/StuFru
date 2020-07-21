1476448a2d047ff0f56c1636c632430eba4fd0ca
HirotoYasuda <qnjmr202@yahoo.co.jp>
2020-07-21 07:48:43 +0000

    feat: file uploading with active storage #10

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,12 @@
 class User < ApplicationRecord
+  has_one_attached :avatar
+  before_create :default_image
+
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :confirmable
+          :recoverable, :rememberable, :validatable,
+          :confirmable
+
+    def default_image
+      self.avatar.attach(io: File.open(Rails.root.join('app', 'assets', 'images', 'avatar-default.png')), filename: 'avatar-dafault.png', content_type: 'image/png')
+    end
 end
