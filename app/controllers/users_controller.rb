cac8a0b2cce9c496eff27efdc26489f4125ac4a8
HirotoYasuda <qnjmr202@yahoo.co.jp>
2020-07-21 07:39:31 +0000

    add: user name editing window with modal #10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,35 @@ class UsersController < ApplicationController
 
   def edit
   end
-end
+
+  def update
+    @user = current_user
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to edit_user_path(@user), notice: 'User was successfully updated.' }
+        format.json { render :index, status: :ok, location: users_path }
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(
+        :name,
+        :avatar,
+        :target_comment,
+        :introduction,
+        :target,
+        :gender,
+        :age,
+        :live,
+        :job,
+        :university,
+        :high_school
+        )
+    end
+  end
