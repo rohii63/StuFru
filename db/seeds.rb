5d8019002f28b6ce174c3ae68162528ea8c48551
HirotoYasuda <qnjmr202@yahoo.co.jp>
2020-07-27 02:38:46 +0000

    fix: target editing window #17

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(
+  email: 'trial@mail',
+  name: 'trial',
+  password: 'password',
+  confirmed_at: '2020-07-25 00:10:00',
+  confirmation_sent_at: '2020-07-25 00:00:00'
+)
