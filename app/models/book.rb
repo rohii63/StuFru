f6d0da6234df5f07b7f6ace462d1d1257e6d96b6
HirotoYasuda <qnjmr202@yahoo.co.jp>
2020-08-11 18:32:48 +0900

    feat: add book timeline in book edit page

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,7 +5,7 @@ class Book < ApplicationRecord
                               foreign_key: "book_id",
                               dependent:  :destroy
   has_many :users, through: :owners
-  has_one :micropost
+  has_many :microposts
   validates :name, presence: true, uniqueness: true
 
   def self.name_search(search)
