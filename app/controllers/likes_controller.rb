5af39598bb3383a39e9b04f97f0b74d601464e61
HirotoYasuda <qnjmr202@yahoo.co.jp>
2020-08-18 12:31:42 +0900

    feat: notification function

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,7 +1,7 @@
 class LikesController < ApplicationController
   def create
     micropost = Micropost.find(params[:micropost_id])
-    current_user.likes.create(micropost_id: micropost)
+    current_user.likes.create(micropost_id: micropost.id)
     micropost.create_notification_like!(current_user)
     redirect_to request.referrer
   end
--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -25,11 +25,11 @@ class Micropost < ApplicationRecord
 
   def create_notification_like!(current_user)
     # すでに「いいね」されているか検索
-    temp = Notification.where(["visitor_id = ? and visited_id = ? and post_id = ? and action = ? ", current_user.id, user_id, id, 'like'])
+    temp = Notification.where(["visitor_id = ? and visited_id = ? and micropost_id = ? and action = ? ", current_user.id, user_id, id, 'like'])
     # いいねされていない場合のみ、通知レコードを作成
     if temp.blank?
       notification = current_user.active_notifications.new(
-        post_id: id,
+        micropost_id: id,
         visited_id: user_id,
         action: 'like'
       )
@@ -43,7 +43,7 @@ class Micropost < ApplicationRecord
 
   def create_notification_comment!(current_user, comment_id)
     # 自分以外にコメントしている人をすべて取得し、全員に通知を送る
-    temp_ids = Comment.select(:user_id).where(post_id: id).where.not(user_id: current_user.id).distinct
+    temp_ids = Comment.select(:user_id).where(micropost_id: id).where.not(user_id: current_user.id).distinct
     temp_ids.each do |temp_id|
       save_notification_comment!(current_user, comment_id, temp_id['user_id'])
     end
@@ -54,7 +54,7 @@ class Micropost < ApplicationRecord
   def save_notification_comment!(current_user, comment_id, visited_id)
     # コメントは複数回することが考えられるため、１つの投稿に複数回通知する
     notification = current_user.active_notifications.new(
-      post_id: id,
+      micropost_id: id,
       comment_id: comment_id,
       visited_id: visited_id,
       action: 'comment'
