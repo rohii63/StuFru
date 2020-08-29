a49ff3a0eaf9709a34eeb3093ee4db904b1ec353
HirotoYasuda <qnjmr202@yahoo.co.jp>
2020-08-29 16:04:48 +0900

    feat: like btn with ajax

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,13 +1,12 @@
 class LikesController < ApplicationController
   def create
-    micropost = Micropost.find(params[:micropost_id])
-    current_user.likes.create(micropost_id: micropost.id)
-    micropost.create_notification_like!(current_user)
-    redirect_to request.referrer
+    @micropost = Micropost.find(params[:micropost_id])
+    current_user.likes.create(micropost_id: @micropost.id)
+    @micropost.create_notification_like!(current_user)
   end
 
   def destroy
     Like.find(params[:id]).destroy
-    redirect_to request.referrer
+    @micropost = Micropost.find(params[:micropost_id])
   end
 end
