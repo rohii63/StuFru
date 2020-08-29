d6ca95d1f460763af1539765401b0698693ae1fc
HirotoYasuda <qnjmr202@yahoo.co.jp>
2020-08-29 18:57:25 +0900

    feat: follow btn with Ajax

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,16 +1,14 @@
 class RelationshipsController < ApplicationController
   
   def create
-    user = User.find(params[:followed_id])
-    current_user.follow(user)
-    user.create_notification_follow!(current_user)
-    redirect_to my_page_path(user)
+    @user = User.find(params[:followed_id])
+    current_user.follow(@user)
+    @user.create_notification_follow!(current_user)
   end
 
   def destroy
-    user = Relationship.find(params[:id]).followed
-    current_user.unfollow(user)
-    redirect_to my_page_path(user)
+    @user = Relationship.find(params[:id]).followed
+    current_user.unfollow(@user)
   end
 
 end
