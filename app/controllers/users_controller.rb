f9cad0c0013a904a210d2747889bf4fc23b7da07
HirotoYasuda <qnjmr202@yahoo.co.jp>
2020-09-04 13:40:14 +0900

    feat: user edit with Ajax

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,10 +17,10 @@ class UsersController < ApplicationController
   def show
     @user = current_user
     @books = @user.books.all
-    @temporaryTargets = []
+    @tmpTargets = []
     8.times do |n|
       n += 1
-      @temporaryTargets.push(Target.new(target_category_id: "#{n}"))
+      @tmpTargets.push(Target.new(target_category_id: "#{n}"))
     end
     @targetContent1 = Target.where(target_category_id:1)
     @targetContent2 = Target.where(target_category_id:2)
@@ -33,31 +33,13 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user = User.find(params[:id])
-      if @user.update(user_params)
-        flash[:success] = "編集完了"
-        redirect_to user_path(@user)
-      else
-        @books = @user.books.all
-        @temporaryTargets = []
-        8.times do |n|
-          n += 1
-          @temporaryTargets.push(Target.new(target_category_id: "#{n}"))
-        end
-        @targetContent1 = Target.where(target_category_id:1)
-        @targetContent2 = Target.where(target_category_id:2)
-        @targetContent3 = Target.where(target_category_id:3)
-        @targetContent4 = Target.where(target_category_id:4)
-        @targetContent5 = Target.where(target_category_id:5)
-        @targetContent6 = Target.where(target_category_id:6)
-        @targetContent7 = Target.where(target_category_id:7)
-        @targetContent8 = Target.where(target_category_id:8)
-        render 'show'
-      end
+    @user = current_user
+    @user.update(user_params)
+    @modal_name = params[:modal][:name]
   end
 
   def follow
-    @user = User.find(params[:id])
+    @user = current_user
     @followers = @user.followers.all
     @following = @user.following.all
     render 'show_follow'
--- app/javascript/packs/users/show.js
+++ app/javascript/packs/users/show.js
@@ -1,18 +1,10 @@
-
 import 'select2'
 import 'select2/dist/css/select2.css'
 
 $(function () {
-  $('#university').select2();
+  $('#user_my_choice_university').select2();
 });
 
-$(function() {
-  $('#university').change(function() {
-    var val = $(this).val();
-    console.log(val);
-    $("#user_my_choice_university").attr("value", val)
-  });
-});
 // window.sendValue = function(){
 //   var selectedValue = $("#university").val();
 //   console.log(selectedValue)
