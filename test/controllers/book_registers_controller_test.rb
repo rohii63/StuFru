3d78a2117cc4094f1c9cc117611bf16ae3489dda
HirotoYasuda <qnjmr202@yahoo.co.jp>
2020-12-11 16:54:07 +0900

    chore: rspec-rails

--- test/controllers/book_registers_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-class BookRegistersControllerTest < ActionDispatch::IntegrationTest
-  test 'should get create' do
-    get book_registers_create_url
-    assert_response :success
-  end
-end
